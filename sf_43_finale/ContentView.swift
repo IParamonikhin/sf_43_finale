d357c01a1df622c5138ad287d8f9c89a0371630b
IParamonikhin <paramonikhin.ivan@yandex.ru>
2023-11-26 15:02:24 +0300

    Initial Commit

---
+++ sf_43_finale/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sf_43_finale
+//
+//  Created by Иван on 26.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sf_43_finale/sf_43_finaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  sf_43_finaleApp.swift
+//  sf_43_finale
+//
+//  Created by Иван on 26.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct sf_43_finaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
